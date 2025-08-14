9ed5b6a468b08e701f254e2d7049d741b18adc9e
ffotre <fred@gmail.com>
2025-08-14 11:02:53 +0200

    Récupérer une seule entité

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -7,16 +7,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/serie', name: 'serie')]
 final class SerieController extends AbstractController
 {
 
-    #[Route('/serie/list', name: 'serie_list', methods: ['GET'])]
+    #[Route('/list', name: '_list', methods: ['GET'])]
     public function list(SerieRepository $serieRepository): Response
     {
         $series = $serieRepository->findAll();
 
         return $this->render('serie/list.html.twig', [
             'series' => $series
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: '_detail')]
+    public function detail(int $id, SerieRepository $serieRepository): Response
+    {
+        $serie = $serieRepository->find($id);
+
+        if (!$serie) {
+            throw $this->createNotFoundException('Pas de série pour cet id');
+        }
+
+        return $this->render('serie/detail.html.twig', [
+            'serie' => $serie
         ]);
     }
 
