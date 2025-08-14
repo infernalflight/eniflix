9f4f1269d3b3f8c16635708ceabbd83cb7cf2eb9
ffotre <fred@gmail.com>
2025-08-14 14:25:07 +0200

    Liste pagination

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\SerieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,14 +12,34 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SerieController extends AbstractController
 {
 
-    #[Route('/list', name: '_list', methods: ['GET'])]
-    public function list(SerieRepository $serieRepository): Response
+    #[Route('/list/{page}', name: '_list', requirements: ['page' => '\d+'], defaults: ['page' => 1], methods: ['GET'])]
+    public function list(SerieRepository $serieRepository, int $page, ParameterBagInterface $parameters): Response
     {
-        $series = $serieRepository->findAll();
+        //$series = $serieRepository->findAll();
+
+        $nbPerPage = $parameters->get('serie')['nb_max'];
+        $offset = ($page - 1) * $nbPerPage;
+        $criterias = [
+            'status' => 'Returning',
+            'genre' => 'Drama',
+        ];
+
+        $series = $serieRepository->findBy(
+            $criterias,
+            ['popularity' => 'DESC'],
+            $nbPerPage,
+            $offset
+        );
+
+        $total = $serieRepository->count($criterias);
+        $totalPages = ceil($total / $nbPerPage);
 
         return $this->render('serie/list.html.twig', [
-            'series' => $series
-        ]);
+                'series' => $series,
+                'page' => $page,
+                'total_pages' => $totalPages,
+            ]
+        );
     }
 
     #[Route('/detail/{id}', name: '_detail')]
