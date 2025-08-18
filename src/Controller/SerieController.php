066939e8d6662843112b5e5cf4155940ae9da2ef
fred <fred@campus.fr>
2025-08-18 15:58:46 +0200

    Form 2e partie - Améliorations

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -87,9 +87,10 @@ final class SerieController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
-            $serie->setDateCreated(new \DateTime());
             $em->persist($serie);
             $em->flush();
+
+            $this->addFlash('success', 'Une série a été enregistrée');
 
             return $this->redirectToRoute('serie_detail', ['id' => $serie->getId()]);
         }
